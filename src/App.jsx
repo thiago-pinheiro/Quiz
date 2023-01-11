c68127b2f0fd0a09b35736b2c901cc7db6f394ab
thiago-pinheiro <dev.thiagopinheiro@gmail.com>
2023-01-11 18:42:51 -0300

    implementing the context in the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import Welcome from './components/welcome'
-import './App.css'
+import Question from "./components/Question";
+import { QuizContext } from "./context/quiz";
+import Welcome from "./components/welcome";
+import "./App.css";
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div className="App">
-     <h1>Quiz de Programação</h1>
-     <Welcome/>
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}{" "}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
